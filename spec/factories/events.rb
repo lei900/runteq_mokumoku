7e20f9a6fa4450b1c32f5aada417b17efb649604
lei <mia.wang90@gmail.com>
2022-09-09 17:50:49 +0900

    Fix lint errors and add platform x86_64-linux

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :event do
     title { Faker::Lorem.word }
     content { Faker::Lorem.paragraph_by_chars(number: 100) }
-    held_at { Time.current + 1.month }
+    held_at { 1.month.from_now }
     prefecture_id { [*1..47].sample }
     user
   end
